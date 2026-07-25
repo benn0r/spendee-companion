52bd0fb4ba99859803d6872f493daf121be0e352
benn0r <benjamin@benn0r.me>
2026-07-25 17:53:08 +0200

    Align daily transaction headers

--- app/DayHeader.tsx
+++ app/DayHeader.tsx
@@ -6,17 +6,19 @@ export default function DayHeader(
   return (
     <tr className="day-header">
       <td colSpan={colSpan}>
-        <strong>{formatDayLabel(day)}</strong>
-        <span>
-          {totals.map((item) => (
-            <b className={item.total < 0 ? "negative" : "positive"} key={item.currency}>
-              {new Intl.NumberFormat("de-CH", {
-                style: "currency",
-                currency: item.currency,
-              }).format(item.total)}
-            </b>
-          ))}
-        </span>
+        <div>
+          <strong>{formatDayLabel(day)}</strong>
+          <span>
+            {totals.map((item) => (
+              <b key={item.currency}>
+                {new Intl.NumberFormat("de-CH", {
+                  style: "currency",
+                  currency: item.currency,
+                }).format(item.total)}
+              </b>
+            ))}
+          </span>
+        </div>
       </td>
     </tr>
   );
